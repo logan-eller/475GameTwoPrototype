7b575cb436c295b9d65804362aba74345616ea10
logan-eller <logan.eller@gmail.com>
2022-10-18 13:15:44 -0400

    Add emmision to mushroom level indicators
    also added enemy models and animations, have to implement still

---
+++ Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MoveTowards : MonoBehaviour
+{
+
+    public GameManager GameManager;
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MinDist = 0;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += Vector3.Scale(transform.forward * MoveSpeed * Time.deltaTime, new Vector3(1.0f, 0.0f, 1.0f));
+
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        GameObject collidedWith = GetComponent<Collider>().gameObject;
+        if (collidedWith.tag == gameObject.tag)
+        {
+            SceneManager.LoadScene(sceneName: "Hallway Level");
+        }
+    }
+}
